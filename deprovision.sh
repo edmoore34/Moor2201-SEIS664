bf89a69c3a701c4060c5dd0de3a33c584521e4f9
Ed Moore <71158828+edmoore34@users.noreply.github.com>
2020-11-14 16:18:27 -0600

    Update deprovision.sh

--- deprovision.sh
+++ deprovision.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcloud compute instances delete -q node-svc-01
-gcloud compute instances delete -q node-svc-02
+gcloud compute instances delete -q node-svc-01 --zone us-central1-c
+gcloud compute instances delete -q node-svc-02 --zone us-central1-c
 gcloud compute firewall-rules delete -q allow-node-svc-tcp-3000
